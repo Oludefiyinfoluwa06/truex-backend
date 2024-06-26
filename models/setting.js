6a749e9c21156df988c0a52a4b895034a5e44286
Timothy <oludefiyinfoluwa06@gmail.com>
2024-06-26 15:11:45 +0100

    Update to setting controller

--- models/setting.js
+++ models/setting.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const SettingSchema = new mongoose.Schema({
-    globalEarningLimit: { type: Number, required: true, default: 40000000 }
+    globalEarningLimit: { type: Number, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Setting', SettingSchema);
